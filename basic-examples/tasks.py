3a123a1a7f1af0ca1a8eb7992c99427d91329511
Jeff Hale <jeffmshale@gmail.com>
2024-03-19 15:52:50 -0400

    change to basic examples;

--- basic-examples/tasks.py
+++ basic-examples/tasks.py
@@ -9,9 +9,7 @@ def my_b_task(name: str):
 
 
 if __name__ == "__main__":
-    from task_server import my_b_task
+    # following line is not necessary if on prefect 2.16.5 or newer
+    from tasks import my_background_task
 
-    serve(my_b_task)
-
-    # NOTE: The serve() function accepts multiple task runs of the same task.
-    # You can start multiple task servers.
+    serve(my_background_task)
--- tutorial/task_submitter.py
+++
@@ -1,5 +0,0 @@
-from task_server import my_b_task
-
-if __name__ == "__main__":
-    val = my_b_task.submit("Agrajag")
-    print(val)
